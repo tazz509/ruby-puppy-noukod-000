dcf25b2bae082bdeaf2a07273b389c6d33f2d74c
carly <carly@noukod.com>
2018-10-03 18:38:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 class Dog
-  attr :name
-
-  @@all = []
-
-  def initialize name
+  attr_accessor :name
+   @@all = []
+   def initialize(name)
     @name = name
-    name
+    @@all << self
   end
+   def self.all
+     @@all.each do |dog|
+      puts dog.name
+    end
+  end
+   def self.clear_all
+    @@all.clear
+  end
+end
